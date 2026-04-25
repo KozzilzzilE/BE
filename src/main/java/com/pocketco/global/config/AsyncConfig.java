93bab48eaeab6fe8d1cfed11edbd76e750653dd3
okjunges <rladhkstn02@gmail.com>
2026-04-25 21:27:12 +0900

    Refactor : 코드 채점 진행도 조절 및 제출 시 바로 스케줄러가 동작할 수 있도록 로직 수정

---
+++ src/main/java/com/pocketco/global/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.pocketco.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean(name = "judge0TriggerExecutor")
+    public Executor judge0TriggerExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("judge0-trigger");
+        executor.initialize();
+        return executor;
+    }
+}
