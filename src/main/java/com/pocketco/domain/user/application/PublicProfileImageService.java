89fd01203808f4774b1b60251450516540c3a731
rlagkdms1109 <rlagkdms1109@naver.com>
2026-05-12 17:43:02 +0900

    feat:공개 프로필 이미지 목록 조회 API 구현

--- src/main/java/com/pocketco/domain/user/application/PublicProfileImageService.java
+++ src/main/java/com/pocketco/domain/user/application/PublicProfileImageService.java
@@ -2,9 +2,11 @@ package com.pocketco.domain.user.application;
 
 import com.pocketco.domain.admin.dto.AddPublicProfileImageResponse;
 import org.springframework.web.multipart.MultipartFile;
+import com.pocketco.domain.user.dto.UserResponseDTO;
 
 import java.io.IOException;
 
 public interface PublicProfileImageService {
     AddPublicProfileImageResponse addPublicProfileImage(MultipartFile image) throws IOException;
+    UserResponseDTO.ProfileImageListResponse getProfileImages();
 }
